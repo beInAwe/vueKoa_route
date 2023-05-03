564c15bf2fee5e955a928203fc1505b315f557c5
wiln <2938193475@qq.com>
2023-05-03 09:24:54 +0800

    --

---
+++ README.md
@@ -0,0 +1,17 @@
+# vueKoa_route
+
+### 项目截图
+
+![](./vueproject/image/1.png)
+
+![](./vueproject/image/2.png)
+
+![](./vueproject/image/3.png)
+
+![](./vueproject/image/4.png)
+
+![](./vueproject/image/5.png)
+
+![](./vueproject/image/6.png)
+
+![](./vueproject/image/7.png)
--- koaserver/db/index.js
+++ koaserver/db/index.js
@@ -1,15 +1,37 @@
 const mysql = require('mysql');
 // 创建连接池
 const pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 20,
     host: '127.0.0.1',
     user: 'root',
     password: 'admin123',
     database: 'my_db_01'
 });
 
-// 定义数据库操作的方法，导出，方便后续操作
-// error:数据库操作错误；result：数据库操作成功后返回的结果
+
+//测试能否正常工作 仅供测试
+pool.query('select 1',(err,results)=>{
+
+    if(err) return console.log(err.message);
+
+    console.log(results);
+})
+
+
+// // 新增数据 username 值为spiderman  password 'pcc123'
+// const user = {username:'spiderman11111' ,password :'pcc123111'}
+
+// const sqlStr ='insert into hk_users(username ,password) value(?,?)'
+
+// pool.query(sqlStr,[user.username,user.password],(err,results)=>{
+//     if(err) return console.log(err.message);
+
+//     //判断是否插入成功
+//     if(results.affectedRows===1){
+//         console.log('插入成功');
+//     }
+// })
+
 function excute(sqlString, condition) {
     return new Promise((res, rej) => {
         pool.query(sqlString, condition, function (error, result, fields) {
@@ -23,13 +45,7 @@ function excute(sqlString, condition) {
     })
 }
 
-//测试能否正常工作 仅供测试
-pool.query('select 1',(err,results)=>{
 
-    if(err) return console.log(err.message);
-
-    console.log(results);
-})
 
 module.exports = {
     EXCUTE: excute
